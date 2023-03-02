fc79bbbbdcc188c891cb9c0c656f22d2a56ffdb6
Daria <darakirichenko@gmail.com>
2023-03-03 01:04:47 +0300

    Task38 is done

---
+++ Desktop/C#_Homework/Sem5/Task38/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int length = ReadNumber("Введите число элементов в массиве:");
+
+double[] GetRandomArray()
+{
+    double[] array = new double[length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.NextDouble();
+    }
+    return array;
+}
+double[] myArray = GetRandomArray();
+Console.WriteLine($"[{string.Join(", ", myArray.Select(x=> Math.Round(x, 3)))}]");
+
+(double, double) MaximumAndMinimumElements(double[] array)
+{
+    double max = array[0];
+    double min = array[1];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max)
+        {
+            max = array[i];
+        }
+        else if(array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return (max, min);
+}
+
+(double maximum, double minimum) = MaximumAndMinimumElements(myArray);
+Console.WriteLine($"Максимальное число = {maximum:f3}; Минимальное число = {minimum:f3}");
+double result = maximum - minimum;
+
+Console.WriteLine($"Разница между макимальным и минимальным значением = {result:f3}");
