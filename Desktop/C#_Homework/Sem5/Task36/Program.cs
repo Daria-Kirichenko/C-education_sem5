12c8026b312357f7c94ba9dade6f214f60cc01d1
Daria <darakirichenko@gmail.com>
2023-03-03 01:04:33 +0300

    Task36 is done

---
+++ Desktop/C#_Homework/Sem5/Task36/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int length = ReadNumber("Введите число элементов в массиве:");
+
+int[] GetRandomArray()
+{
+    int[] array = new int[length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(0,1000); //рандомное ограничение, т.к. выдавал большие числа
+    }
+    return array;
+}
+int[] myArray = GetRandomArray();
+Console.WriteLine($"[{string.Join(", ", myArray)}]");
+
+int SummaryOfOddElements(int[] array)
+{
+    int sumOdd = 0;
+    for(int i = 1; i < array.Length; i = i + 2)
+    {
+        sumOdd = sumOdd + array[i];
+    }
+    return sumOdd;
+}
+
+int sumOddElements = SummaryOfOddElements(myArray);
+Console.WriteLine($"Сумма элементов на нечетных позициях = {sumOddElements}");
